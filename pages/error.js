2c644a7c08d78dcd1c553467dbfb0ccfbbc32994
FreeCoderKR <cystmax@gmail.com>
2022-01-02 00:26:58 +0900

    update explanation

--- pages/error.js
+++ pages/error.js
@@ -1,8 +1,9 @@
 import { useEffect } from "react";
 import Router from "next/router";
+//signIn error will arrive here
 export default function Error() {
   useEffect(() => {
     setTimeout(() => Router.push("/"), 2000);
   }, []);
-  return <div>This is Error Page! You will redirected to main page in 2 sec</div>;
+  return <div>SignIn unavailable! You will redirected to main page in 2 sec</div>;
 }
