907ee2a87560553b20340afc1d68e8d10bcb0373
ywj3493 <ywj3493@nate.com>
2022-01-12 19:11:20 +0900

    20220112 main search filter 1st

--- pages/main.js
+++ pages/main.js
@@ -24,6 +24,89 @@ function CheckInOutDateSearch() {
   return <div></div>;
 }
 
+function HeadCountSearch() {
+  const [adultsCount, setAdultsCount] = useState(0);
+  const [childrenCount, setChildrenCount] = useState(0);
+  const [infantsCount, setInfantsCount] = useState(0);
+  const [petsCount, setPetsCount] = useState(0);
+  const [sumAdultsChildrenCount, setSumAdultsChildrenCount] = useState(0);
+
+  useEffect(() => {
+    setSumAdultsChildrenCount(adultsCount + childrenCount);
+  }, [adultsCount, childrenCount]);
+
+  return (
+    <div className="bg white rounded">
+      <div className="border-y-1 border-gray-300">
+        <div className="">{"성인"}</div>
+        <button
+          class="border-1"
+          disabled={adultsCount == 0}
+          onClick={() => setAdultsCount(adultsCount - 1)}
+        >
+          -
+        </button>
+        <div>{adultsCount}</div>
+        <button
+          class="border-1"
+          onClick={() => setAdultsCount(adultsCount + 1)}
+        >
+          +
+        </button>
+      </div>
+      <div className="border-y-1 border-gray-300">
+        <div className="">{"어린이"}</div>
+        <button
+          class="border-1"
+          disabled={childrenCount == 0}
+          onClick={() => setChildrenCount(childrenCount - 1)}
+        >
+          -
+        </button>
+        <div>{childrenCount}</div>
+        <button
+          class="border-1"
+          onClick={() => setChildrenCount(childrenCount + 1)}
+        >
+          +
+        </button>
+      </div>
+      <div className="border-y-1 border-gray-300">
+        <div className="">{"유아"}</div>
+        <button
+          class="border-1"
+          disabled={infantsCount == 0}
+          onClick={() => setInfantsCount(infantsCount - 1)}
+        >
+          -
+        </button>
+        <div>{infantsCount}</div>
+        <button
+          class="border-1"
+          onClick={() => setInfantsCount(infantsCount + 1)}
+        >
+          +
+        </button>
+      </div>
+      <div className="border-y-1 border-gray-300">
+        <div className="">{"반려동물"}</div>
+        <button
+          class="border-1"
+          disabled={petsCount == 0}
+          onClick={() => setPetsCount(petsCount - 1)}
+        >
+          -
+        </button>
+        <div>{petsCount}</div>
+        <button class="border-1" onClick={() => setPetsCount(petsCount + 1)}>
+          +
+        </button>
+        <div>{`총 ${sumAdultsChildrenCount} 명`}</div>
+      </div>
+    </div>
+  );
+}
+
 function MainSearchBar() {
   const [location, setLocation] = useState("");
   const [checkInDate, setCheckInDate] = useState("");
@@ -54,6 +137,15 @@ function MainSearchBar() {
     });
   };
 
+  const onClickHeadCountSearch = () => {
+    setPopState({
+      location: false,
+      checkIn: false,
+      checkOut: false,
+      headCount: true,
+    });
+  };
+
   return (
     <div className="bg-white ">
       <div classNmae="bg-white ">
@@ -75,13 +167,14 @@ function MainSearchBar() {
         <TextField
           label="인원"
           variant="standard"
-          onClick={setHeadCount}
+          onClick={onClickHeadCountSearch}
         ></TextField>
         <button>검색</button>
       </div>
       {popState.location ? <LocationSearch /> : null}
       {popState.checkIn ? <CheckInOutDateSearch /> : null}
       {popState.checkOut ? <CheckInOutDateSearch /> : null}
+      {popState.headCount ? <HeadCountSearch /> : null}
     </div>
   );
 }
