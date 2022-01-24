ea7408f7f86db5e6b9af8b9d6d88eb3435d41c16
FreeCoderKR <cystmax@gmail.com>
2022-01-24 23:56:07 +0900

    update useMediaquery no error

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,31 @@
+import Head from "next/head";
 import Link from "next/link";
-
+import { useEffect, useState } from "react";
+import { useMediaQuery } from "react-responsive";
+import MobileHome from "../components/Home/MobileHome";
+import PCHome from "../components/Home/PCHome";
 export default function MainPage() {
+  const isMobile = useMediaQuery({
+    maxWidth: 768,
+  });
+
   return (
-    <div className="flex-col">
-      <div className="bg-gray-500 text-center p-5 rounded-xl text-white ">This is MainPage!</div>
-      <div className="flex space-x-5 mt-10">
-        <Link href="/public" passHref>
-          <button className="underline">Go to Public Page</button>
-        </Link>
-        <Link href="/private" passHref>
-          <button className="underline">Go to Private Page</button>
-        </Link>
-      </div>
-    </div>
+    // <div className="flex-col">
+    //   <div className="p-5 text-center text-white bg-gray-500 rounded-xl ">This is MainPage!</div>
+    //   <div className="flex mt-10 space-x-5">
+    //     <Link href="/public" passHref>
+    //       <button className="underline">Go to Public Page</button>
+    //     </Link>
+    //     <Link href="/private" passHref>
+    //       <button className="underline">Go to Private Page</button>
+    //     </Link>
+    //   </div>
+    // </div>
+    <>
+      <Head>
+        <title>AirBnb Project</title>
+      </Head>
+      {isMobile ? <MobileHome /> : <PCHome />}
+    </>
   );
 }
